6d7a2fc14d0525805f3afe9e00dd07d1848413ea
陈昌熙 <ccx1016875161@gmail.com>
2019-09-26 18:12:07 +0800

    Maven分模块的SpringBoot项目调试完毕

---
+++ ccx-interact/src/main/java/com/ccx/interact/CCXApplication.java
@@ -0,0 +1,20 @@
+package com.ccx.interact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: com.ccx.interact
+ * @description: 这是SpringBoot的启动类
+ * @authhor: ChenChangxi
+ * @create: 2019-09-26 17:59
+ **/
+
+@SpringBootApplication
+public class CCXApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(CCXApplication.class,args);
+    }
+}
